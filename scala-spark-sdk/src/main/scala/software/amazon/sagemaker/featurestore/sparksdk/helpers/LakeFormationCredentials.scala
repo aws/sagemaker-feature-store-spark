9eb2051bc105d0a34d7dee20ba5ee6026b76c885
Basssem Halim <bhhalim@amazon.com>
2026-04-07 14:24:16 -0700

    feat: add LakeFormation credentials model and ARN resolver extensions
    Add LakeFormationCredentials case class to hold LF-vended temporary
    credentials with isExpiringSoon() for refresh logic. Extend
    FeatureGroupArnResolver with resolveAccountId() and resolvePartition()
    to extract account ID and ARN partition from feature group ARNs.
    ---
    X-AI-Prompt: add lake formation managed table support to spark connector
    X-AI-Tool: kiro-cli

---
+++ scala-spark-sdk/src/main/scala/software/amazon/sagemaker/featurestore/sparksdk/helpers/LakeFormationCredentials.scala
@@ -0,0 +1,34 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *      http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+package software.amazon.sagemaker.featurestore.sparksdk.helpers
+
+import java.time.Instant
+
+case class LakeFormationCredentials(
+    accessKeyId: String,
+    secretAccessKey: String,
+    sessionToken: String,
+    expiration: Instant,
+    region: String,
+    accountId: String,
+    database: String,
+    table: String
+) {
+  def isExpiringSoon(bufferSeconds: Int): Boolean = {
+    Instant.now().plusSeconds(bufferSeconds).isAfter(expiration)
+  }
+}
